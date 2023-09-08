7aac18ce2ea0ec693fbdb3f73f0196d37a4594ca
Sandiso <sandiso.jele@gmail.com>
2023-09-08 07:55:07 -0700

    7 & 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers & letters 
+ * numbers etween 0-9, with letters a to f
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
