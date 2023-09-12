f7efde559df25f0dfedf2e5c84b59b19e52d237f
Sandiso <sandiso.jele@gmail.com>
2023-09-12 12:59:53 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times in lowercase
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'a' ; j++)
+			_putchar(j);
+	}
+	_putchar('\n');
+}
