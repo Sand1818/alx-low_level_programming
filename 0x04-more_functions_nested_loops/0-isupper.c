d02dd9574fa01346f921af4353500735bbc9e100
Sandiso <sandiso.jele@gmail.com>
2023-09-14 08:00:53 -0700

    task 1 Check uppercase char

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *  @c: character  being checked
+ * Return: 1 if @c is uppercase, otherwise 0
+**/
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
