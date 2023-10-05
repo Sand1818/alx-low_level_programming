1b0ec9718391137d1357901540469a5c8114aa39
Sandiso <sandiso.jele@gmail.com>
2023-10-05 03:14:52 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: unsigned int
+ * Return: pointer to new memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (s == NULL)
+		exit(98);
+	return (s);
+}
