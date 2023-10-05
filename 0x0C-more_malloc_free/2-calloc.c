0a290121441cf57fe9092746a4707633e6fc849c
Sandiso <sandiso.jele@gmail.com>
2023-10-05 04:06:44 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array with malloc
+ * @nmemb: array
+ * @size: size
+ * Return: adress of allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *cat;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	cat = malloc(nmemb * size);
+	return (cat);
+}
