444e8f2735fdbdc42e01206a5e2f0fcb622fc7a4
Sandiso <sandiso.jele@gmail.com>
2023-10-06 06:31:48 -0700

    task 2 attempt 3 'i think'

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,10 +3,30 @@
 #include <stdio.h>
 
 /**
- * _calloc - allocates memory for an array with malloc
+ * *_memset - fills memory with a constant byte
+ * @s: memory
+ * @b: copied char
+ * @n: num of copies
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
  * @nmemb: array
- * @size: size
- * Return: adress of allocated memory
+ * @size: size element
+ * Return: adress  allocated memory
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -16,10 +36,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	cat = malloc(nmemb * size);
+	cat = malloc(size * nmemb);
 
 	if (cat == NULL)
 		return (NULL);
-	else
-		return (cat);
+
+	_memset(cat, 0, nmemb * size);
+
+	return (cat);
 }
