4271baf9c9d4539b8b1cb45115bd89ad41634727
Sandiso <sandiso.jele@gmail.com>
2023-10-09 13:17:54 -0700

    task 0 poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
