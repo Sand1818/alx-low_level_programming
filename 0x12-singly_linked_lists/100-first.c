f9203458997f67486f9befa95fba6f87c1b67be3
Sandiso <sandiso.jele@gmail.com>
2023-10-19 07:27:48 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * f_first - prints
+ */
+
+void  __attribute__ ((constructor)) f_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
